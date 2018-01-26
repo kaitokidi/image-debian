734c0e7f3a8e6fa9cbe9868f41328cd22b25cfc9
Maël Le Garrec <mlegarrec@online.net>
2018-01-26 14:09:52 +0000

    update sid to the new build system

---
+++ sid/env.mk
@@ -0,0 +1,9 @@
+IMAGE_NAME = debian
+IMAGE_VERSION = sid
+IMAGE_VERSION_ALIASES = unstable
+IMAGE_TITLE = Debian Sid/Unstable
+IMAGE_DESCRIPTION = Debian Sid/Unstable
+IMAGE_SOURCE_URL = https://github.com/scaleway/image-debian
+IMAGE_VENDOR_URL = https://wiki.debian.org/DebianUnstable
+IMAGE_BOOTSCRIPT = mainline 4.9
+IMAGE_BASE_FLAVORS = common docker-based systemd feature-motd
